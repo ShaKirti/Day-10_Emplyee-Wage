a6d76767db180bace9f8593a01e1b3a75a805637
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-20 22:32:10 +0530

    Added: Logic for calculating employee wage present for the part time.

--- src/com/employee/company/EmployeeAttendance.java
+++ src/com/employee/company/EmployeeAttendance.java
@@ -4,17 +4,23 @@ public class EmployeeAttendance {
     public static void main(String[] args) {
         int WagePerHour = 20;
         int FullDayHour = 8;
+        int HalfDayHour = 4;
         int TotalWage = 0;
         System.out.println("Welcome to Employee Wage Computation");
 
-        int randomCheck = new Random(2).nextInt();
+        int randomCheck = new Random(3).nextInt();
 
         if (randomCheck == 0) {
             System.out.println("Employee is absent");
         }
-        else {
+        else if(randomCheck == 1){
             TotalWage = WagePerHour * FullDayHour;
             System.out.println("Employee is present");
+            System.out.println(TotalWage);
+        }
+        else{
+            TotalWage = WagePerHour * HalfDayHour;
+            System.out.println("Employee is present for the half day.");
             System.out.println(TotalWage);
         }
     }
