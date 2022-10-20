f722d5c806abd3ef4aa1525f2a6578b6e5a4a7a8
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-21 02:17:56 +0530

    Added:Daily wage along with total wage.

--- src/com/employee/company/CompanyInfo.java
+++ src/com/employee/company/CompanyInfo.java
@@ -1,10 +1,14 @@
 package com.employee.company;
 
+import java.util.Arrays;
+
 public class CompanyInfo {
     public final String companyName;
     public final int empRatePerHr;
     public final int numberOfWorkingDays;
     public final int maxWorkingHrsPerMonth;
+    public int totalEmpWage = 0;
+    public int[] dailyWage = new int[20];
 
     public CompanyInfo(String companyName, int empRatePerHr, int numberOfWorkingDays, int maxWorkingHrsPerMonth) {
         super();
@@ -12,6 +16,11 @@ public class CompanyInfo {
         this.empRatePerHr = empRatePerHr;
         this.numberOfWorkingDays = numberOfWorkingDays;
         this.maxWorkingHrsPerMonth = maxWorkingHrsPerMonth;
+    }
+
+    @Override
+    public String toString() {
+        return "Company [totalEmpWage=" + totalEmpWage + "]\n" + "dailyWage=" + Arrays.toString(dailyWage);
     }
 
 }
--- src/com/employee/company/EmployeeBuilder.java
+++ src/com/employee/company/EmployeeBuilder.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class EmployeeBuilder {
     final static int IS_FULL_TIME = 1;
     final static int IS_PART_TIME = 2;
-
+    // instance variables
     ArrayList<CompanyInfo> companyList = new ArrayList<CompanyInfo>();
-
+    
     public void addCompanyDetailsForEmpWage(String companyName, int empRatePerHr, int numnberOfWorkingDays,
                                             int maxWorkingHrsPerMonth) {
         CompanyInfo company = new CompanyInfo(companyName, empRatePerHr, numnberOfWorkingDays, maxWorkingHrsPerMonth);
@@ -17,6 +17,7 @@ public class EmployeeBuilder {
     public void companyEmpWage() {
         for (int i = 0; i < companyList.size(); i++) {
             empWgaeComputation(this.companyList.get(i));
+            System.out.println(this.companyList.get(i).toString());
         }
     }
 
@@ -39,17 +40,13 @@ public class EmployeeBuilder {
     public void empWgaeComputation(CompanyInfo company) {
         int days = 0;
         int totalWorkingHrs = 0;
-        int totalEmpWage = 0;
-
         while (days < company.numberOfWorkingDays && totalWorkingHrs <= company.maxWorkingHrsPerMonth) {
             days++;
-
             int empHrs = this.getEmpHrs();
             totalWorkingHrs += empHrs;
-
-            System.out.println("emp hrs : " + empHrs);
+            company.dailyWage[days] = empHrs * company.empRatePerHr;
         }
-        totalEmpWage = totalWorkingHrs * company.empRatePerHr;
-        System.out.println("Total employee wage for company " + company.companyName + " is " + totalEmpWage);
+        company.totalEmpWage = totalWorkingHrs * company.empRatePerHr;
     }
+
 }
