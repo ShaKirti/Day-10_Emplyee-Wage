7f187fbc1823b6f32ccd10c642616ab9d47c4412
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-20 22:27:16 +0530

    Added: Logic for calculating employee wage

--- src/com/employee/company/EmployeeAttendance.java
+++ src/com/employee/company/EmployeeAttendance.java
@@ -2,6 +2,9 @@ package com.employee.company;
 import java.util.Random;
 public class EmployeeAttendance {
     public static void main(String[] args) {
+        int WagePerHour = 20;
+        int FullDayHour = 8;
+        int TotalWage = 0;
         System.out.println("Welcome to Employee Wage Computation");
 
         int randomCheck = new Random(2).nextInt();
@@ -10,7 +13,9 @@ public class EmployeeAttendance {
             System.out.println("Employee is absent");
         }
         else {
+            TotalWage = WagePerHour * FullDayHour;
             System.out.println("Employee is present");
+            System.out.println(TotalWage);
         }
     }
 }
