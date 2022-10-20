584b7e96dac49f42fe6c02b4206f54c88cd494ee
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-20 22:19:55 +0530

    Added: Logic for attendance.

--- src/com/employee/company/EmployeeAttendance.java
+++ src/com/employee/company/EmployeeAttendance.java
@@ -1,8 +1,16 @@
 package com.employee.company;
-
+import java.util.Random;
 public class EmployeeAttendance {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
+        int randomCheck = new Random(2).nextInt();
+
+        if (randomCheck == 0) {
+            System.out.println("Employee is absent");
+        }
+        else {
+            System.out.println("Employee is present");
+        }
     }
 }
