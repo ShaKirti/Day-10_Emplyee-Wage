7409fa0ede708e50aa32d096b9fb9f652d6185b0
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-21 00:04:51 +0530

    Added: Logic for calculating monthly salary.

--- src/com/employee/company/EmployeeAttendance.java
+++ src/com/employee/company/EmployeeAttendance.java
@@ -1,25 +1,34 @@
 package com.employee.company;
 import java.util.Random;
 public class EmployeeAttendance {
-    public static void main(String[] args) {
+    public static void main(String args[]){
+        System.out.println("Welcome to Employee Wage Computation Program.");
+
         int WagePerHour = 20;
         int FullDayHour = 8;
         int HalfDayHour = 4;
+        int WorkingDayPerMonth = 20;
+        int monthlySalary = 0;
         int TotalWage = 0;
-        System.out.println("Welcome to Employee Wage Computation");
+        
+        int randomCheck = new Random().nextInt(3);
 
-        int randomCheck = new Random(3).nextInt();
-        switch (randomCheck){
-            case 0: System.out.println("Employee is absent.");
-                break;
-            case 1: System.out.println("Employee is present.");
-                TotalWage = WagePerHour * FullDayHour;
-                break;
-            case 2: System.out.println("Employee is present for the half day.");
-                TotalWage = WagePerHour * HalfDayHour;
-                break;
+        for (int i = 0; i < WorkingDayPerMonth; i++) {
+            switch (randomCheck) {
+                case 0:
+                    System.out.println("Employee is absent.");
+                    break;
+                case 1:
+                    System.out.println("Employee is present.");
+                    TotalWage = WagePerHour * FullDayHour;
+                    break;
+                case 2:
+                    System.out.println("Employee is present for the half day.");
+                    TotalWage = WagePerHour * HalfDayHour;
+                    break;
+            }
+            monthlySalary += TotalWage;
         }
-        System.out.println("Employee Salary is: "+TotalWage);
-        }
-
+        System.out.println("Employee Monthly Salary is: "+monthlySalary);
+    }
 }
