86bff65004ebd1709e51c97e31d74e1efd26af67
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-20 22:16:50 +0530

    Added: Welcome meassage

---
+++ src/com/employee/company/EmployeeAttendance.java
@@ -0,0 +1,8 @@
+package com.employee.company;
+
+public class EmployeeAttendance {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+    }
+}
